cc5e87e308962f2dc5ef7d76389dc97e95417c78
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-31 21:13:09 +0530

    Added header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Header=()=>{
+return(
+    <div>
+        <h1>Medical Shop With Stock Management</h1>
+        <button >Cart</button>
+    </div>
+);
+};
+export default Header;
