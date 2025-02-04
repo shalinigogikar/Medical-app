ae54a5224caa2df03072f40e60555290a0c6dc62
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-04 13:08:43 +0530

    header added with card handling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React,{useContext, useState}from "react";
+import BillContext from "./BillContext";
 const Header=()=>{
+    const {bill}=useContext(BillContext);
+    const[showCart,setShowCart]=useState(false);
+    const cartHandler=()=>
+    {
+        setShowCart((prevBill)=>!prevBill);
+    }
+    let totalAmount = 0;
+for (let i = 0; i < bill.length; i++) {
+  totalAmount += Number(bill[i].price);
+}
 return(
     <div>
         <h1>Medical Shop With Stock Management</h1>
-        <button >Cart</button>
+        <button onClick={cartHandler}>Cart has {bill.length} medicines </button>
+        {showCart&&(
+            <div>
+                {bill.length===0?(
+                    <p>Cart is empty</p>
+                ):(
+                    bill.map((med,index) => (
+            <div key={index}>
+                <p>
+                    {med.medicineName}-{med.price}
+                </p>
+            </div>
+        
+        ))
+        )}
+        <p>Total={totalAmount}</p>
+        </div>)}
     </div>
 );
 };
