e6df4d96766a26fa4db9188d568ae44e28ca3e46
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-04 13:13:41 +0530

    medicines get printed in the medicine list

--- src/components/MedicineList.js
+++ src/components/MedicineList.js
@@ -1,23 +1,56 @@
-import React from "react";
+import React,{useContext, useState}from "react";
+import BillContext from "./BillContext";
 import AddForm from "./AddForm";
-const MedicineList=(props)=>
+const MedicineList=()=>
 {
+    const {bill,setBill}=useContext(BillContext);
+    const [medilist,setMediList]=useState([]);
+    const addMedicineHandler=(medicine)=>{
+        setMediList((prevMedicines) => [...prevMedicines, medicine]);
+  };
+  const addToBillHandler=(med)=>{
+            if(med.quantityAvailable>0)
+            {
+                const updatedMedicine={...med,quantityAvailable:med.quantityAvailable-1};
+                setMediList((prevMedicines)=>
+                    prevMedicines.map((medicine)=>
+                        medicine.medicineName===updatedMedicine.medicineName?updatedMedicine:medicine
+            ));
+                setBill((prevBill)=>[...prevBill,updatedMedicine]);
+
+        }
+    };
+    let totalAmount = 0;
+for (let i = 0; i < bill.length; i++) {
+  totalAmount += Number(bill[i].price);
+}
     return(
         <div>
+            <AddForm onAddMedicine={addMedicineHandler}/>
 <table>
     <thead>
+        <tr>
         <th>Medicine</th>
         <th>Description</th>
         <th>Price</th>
         <th>Quantity Available</th>
+        </tr>
     </thead>
     <tbody>
-        <th>{props.medicineName}</th>
-        <th>{props.Description}</th>
-        <th>{props.price}</th>
-        <th>{props.QuantityAvailable}</th>
+    {medilist.map((med, index) => (
+        <tr key={index}>
+        <td>{med.medicineName}</td>
+        <td>{med.description}</td>
+        <td>{med.price}</td>
+        <td>{med.quantityAvailable}</td>
+        <td>
+            <button onClick={()=>addToBillHandler(med)} disabled={med.quantityAvailable===0}>{med.quantityAvailable===0?"Out Of Stock":"Add To Bill"}</button>
+        </td>
+        </tr>
+        ))}
     </tbody>
 </table>
-</div>);
+</div>
+);
 };
 export default MedicineList;
