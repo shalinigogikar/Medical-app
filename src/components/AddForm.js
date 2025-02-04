f485385ce5d43d312311ee3a7e8ca46c2c4aa2ec
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-04 13:12:38 +0530

    Addform contains input and change Handlers

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,35 +1,69 @@
 import React,{useState} from "react";
-const AddForm=()=>{
+const AddForm=(props)=>{
  const [medicine,setMedicine]=useState({
-    medicineName: "",
-    description: "",
+   medicineName: "",
+   description: "",
     price: "",
     quantityAvailable: "",
  });
-    const addFormHandler=(event)=>{
+ const medicineNameChangeHandler=(event)=>
+    {
+        setMedicine((prevState)=>{
+            return{
+                ...prevState,medicineName:event.target.value};
+            });
+    };
+    const descriptionChangeHandler=(event)=>
+        {
+            setMedicine((prevState)=>{
+                return{
+                    ...prevState,description:event.target.value};
+                });
+        };
+        const priceChangeHandler=(event)=>
+            {
+                setMedicine((prevState)=>{
+                    return{
+                        ...prevState,price:event.target.value};
+                    });
+            };
+            const quantityAvailableChangeHandler=(event)=>
+                {
+                    setMedicine((prevState)=>{
+                        return{
+                            ...prevState,quantityAvailable:event.target.value};
+                        });
+                };
+    const submitFormHandler=(event)=>{
         event.preventDefault();
-        setMedicine(event.target.value);
-    }
+        props.onAddMedicine(medicine);
+        setMedicine({
+            medicineName:'',
+            description:'',
+            price:'',
+            quantityAvailable:'',
+        });
+    };
     return(
-        <form>
+        <form onSubmit={submitFormHandler}>
             <div>
                 <label htmlFor="medicine">medicineName</label>
-                <input type="text" id="medicine"/>
+                <input type="text" value={medicine.medicineName} id="medicine" onChange={medicineNameChangeHandler}/>
             </div>
             <div>
                 <label htmlFor="desc">Description</label>
-                <input type="text" id="desc"/>
+                <input type="text" value={medicine.description} id="desc"onChange={descriptionChangeHandler}/>
             </div>
             <div>
                 <label htmlFor="amount">price</label>
-                <input type="number" id="amount"/>
+                <input type="number" value={medicine.price} id="amount" onChange={priceChangeHandler}/>
             </div>
             <div>
                 <label htmlFor="quantity">QuantityAvailable</label>
-                <input type="number" id="quantity"/>
+                <input type="number" value={medicine.quantityAvailable}id="quantity" onChange={quantityAvailableChangeHandler}/>
             </div>
             <div>
-                <button type="submit" onClick={addFormHandler}>Add</button>
+                <button type="submit">Add</button>
             </div>
         </form>
     );
